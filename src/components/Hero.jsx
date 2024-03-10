85da2dc4113fef9d1f4747a5e0fcaf52b8d43c5d
richz <richarmiguelrodriguez@gmail.com>
2024-03-10 01:02:14 -0500

    hero_section_2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,18 +17,21 @@ const Hero = () => {
       <div className='h-[500px] relative '>
         <img src={heroImg} alt="Imagen for hero" className='w-full h-full object-cover rounded-md ' />
         <div className='absolute bottom-5 w-full '>
-          <div className='w-11/12 m-auto bg-white grid grid-cols-2 justify-between items-center lg:flex xl:w-4/5 '> 
-            <div>
+          <div className='w-11/12 m-auto bg-white grid grid-cols-2 justify-between items-center p-5 drop-shadow-2xl md:space-x-4 md:rounded-md lg:flex xl:w-4/5 '> 
+            <div className='flex flex-col space-y-2 '>
               <label htmlFor="location">Location</label>
-              <input type="text" placeholder='US' />
+              <input className='border-gray-300 border-2 rounded pl-2 outline-0 text-sm ' type="text" placeholder='US' />
             </div>
-            <div>
+            <div className='flex flex-col space-y-2 '>
               <label htmlFor="property">Property</label>
-              <input type="text" placeholder='Property' />
+              <input className='border-gray-300 border-2 rounded pl-2 outline-0 text-sm ' type="text" placeholder='Property' />
+            </div>
+            <div className='flex flex-col space-y-2 '>
+              <label htmlFor="price">Max Price</label>
+              <input className='border-gray-300 border-2 rounded pl-2 outline-0 text-sm ' type="text" placeholder='$100,000' />
             </div>
             <div>
-              <label htmlFor="price">Max Price</label>
-              <input type="text" placeholder='$100,000' />
+              <button className='bg-yellow-400 px-5 py-2 mt-5 rounded-xl md:mt-0 md:rounded-full ' >Search</button>
             </div>
           </div>
         </div>
